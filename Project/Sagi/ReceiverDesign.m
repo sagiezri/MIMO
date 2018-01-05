449401ed7ec7c1c5072acbb3f7cacdb33cafb5e8
Sagi <sagie23@gmail.com>
2018-01-06 00:45:33 +0200

    ReceiverDesign - ver1.3
    ReceiverDesign - ver1.3
    *fixed the normalized values
    *create a testing env for examine results

--- Project/Sagi/ReceiverDesign.m
+++ Project/Sagi/ReceiverDesign.m
@@ -1,45 +1,47 @@
 %*************************************************************************%
 %Receiver Design function - roles:
-%Calculate for each reciver the w vector
+%Calculate for each reciver the w_i vector
 %Input: H_i(matrix - NXM),M(scalar),Receiver_type(scalar),Noise_var(scalar),i(scalar).
 %Output:w_i.
 %*************************************************************************%
 
 function [w_i]=ReceiverDesign(H_i,M,Receiver_type,Noise_var,i)
+N=size(H_i,1);
 %init w_i vector
-w_i=zeros(1,size(H_i,1));
-%Gets h_i vector
+w_i=zeros(1,N);
+%gets h_i vector
 h_i=H_i(:,i);
 %Check if we're on PZF mode
 if Receiver_type~=0
-    %Sorting H_i according minimal norm by the columns ->Calculate H_i_c     
-     H_i_c=H_i;
+    %Sorting H_i according to minimal norm by the columns     
+     H_i_no_hi=H_i;
      %Exclude h_i from the sorting
-     H_i_c(:,i)=[];
-     %Calculate each row norm - to get the closet 
-     minNormPointer=zeros(size(H_i_c,1),1);
-     minNormPointer(:,1)=abs((sqrt(sum(H_i_c.*H_i_c,2))));
-     [minNormPointer(:,2),minNormPointer(:,1)]=sort(minNormPointer(:,1));
-     %Put h_i in the first place
-     H_i_c(:,1)=h_i;
-     H_i_c(:,1:size(H_i_c,1))=H_i_c(:,minNormPointer(:,1));
-     %All the rest sorted columns after    
-     %We've in each row the w_i vector of the crosspding index.
-     %1-MRC,2 and up PZF
-     e_i=zeros(1,Receiver_type);
+     H_i_no_hi(:,i)=[];
+     %Calculate each column norm - to get the closet tramister - we can cancel up to N-1 interference 
+     minNormPointer=zeros(1,M);
+     minNormPointer=abs((sqrt(sum(H_i_no_hi.*H_i_no_hi))));
+     [~,minNormIndex]=sort(minNormPointer(1,:));
+     %Put h_i in the first place -> MRC ->PZF..
+     H_i_rt=zeros(N,N);
+     H_i_rt(:,1)=h_i;
+     %All the rest sorted columns after
+     H_i_rt(:,2:end)=H_i(:,minNormIndex(1:N-1));
+     %h_l always at the first index
+     e_i=zeros(Receiver_type,1);
      e_i(1,1)=1;
-     if Receiver_type==1
-         H_cc=h_i;
-     else
-       for  c=2:Receiver_type
-         H_cc=H_i_c(:,1:c);         
-       end
-     end  
-        w_i=H_cc/(H_cc'*H_cc)*e_i';     
+     %create H_c_i matrix
+       for  c=1:Receiver_type
+         H_c_i=H_i_rt(:,1:c);         
+       end 
+        w_i=H_c_i/(H_c_i'*H_c_i)*e_i;     
+        %return a row vector
+        w_i=w_i.';
 else
 %Receiver_type=0 - MMSE
 H_HH=H_i*H_i';
-w_i=((H_HH+Noise_var.*eye(size(H_HH,2)))\h_i).';
+w_i=((H_HH+Noise_var.*eye(size(H_HH,2)))\h_i);
+%return a row vector
+w_i=w_i.';
 end
     
 end
