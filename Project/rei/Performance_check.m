25fa00e15e4592c5df688f8d611ba3aa207aca23
richterei <richterei@gmail.com>
2018-01-08 18:40:47 +0200

    Third migreation - Project
    Third migreation - Project
    Normalized
    Change H_i enter to PC

--- Project/rei/Performance_check.m
+++ Project/rei/Performance_check.m
@@ -1,15 +1,16 @@
 function [SNR] = Performance_check(Receiver_type,w_i,H_i,i,NoiseVar)
-    if Receiver_type~=0
-        i=1;
+    if Receiver_type ~= 0
+        i = 1;
     end
+    w_i=w_i';
     SNR_denominator_ai=0;SNR_numerator_ai=0;W_i_denominator=0;
-    a_i= w_i'*H_i;    
-    SNR_numerator_ai=norm(a_i(i)).^2;
-    SNR_denominator_ai=sum(norm(a_i).^2);
+    a_i= w_i*H_i;    
+    SNR_numerator_ai=(abs(a_i(i))).^2;
+    SNR_denominator_ai=sum((abs(a_i)).^2);
     %Using the numerator value to calculate the second element in the
     %denominator to avoid "for" loops and condintions
     SNR_denominator_ai=SNR_denominator_ai-SNR_numerator_ai;
-    W_i_denominator= NoiseVar.*(norm(w_i).^2);
+    W_i_denominator= NoiseVar.*sum(abs(w_i).^2);
     SNR=(SNR_numerator_ai)/(W_i_denominator+SNR_denominator_ai);
     
 end
--- Project/rei/main.m
+++ Project/rei/main.m
@@ -1,8 +1,8 @@
 close all;
 clear all;
 %Prameters
-lambda=0.006;lambda_b=0.001;M_avg = 60;B_avg=10;
-L=5;a=2;K=4;
+lambda=0.002;lambda_b=0.001;M_avg = 200;B_avg=100;
+L=3;a=2;K=50;
 SNRdB_rng=-30:10:60;noise_var_vec=10.^(-SNRdB_rng/10);
 Num_of_runs = 1;Num_noise_var = length(noise_var_vec);
 N=K*L;
@@ -15,8 +15,8 @@ for index_runs = 1:Num_of_runs
 		H_i = select_H_i(B,L,M,i,K,H,r_ij);
 		for Receiver_type = 0:N
 			for Noise_var = 1:length(noise_var_vec)
-				w_i = ReceiverDesign(H_i,M,Receiver_type,noise_var_vec(Noise_var),i);
-				SINR_i = Performance_check(Receiver_type,w_i.',H_i,i,noise_var_vec(Noise_var));
+				[w_i,H_to_PC] = ReceiverDesign(H_i,M,Receiver_type,noise_var_vec(Noise_var),i);
+				SINR_i = Performance_check(Receiver_type,w_i.',H_to_PC,i,noise_var_vec(Noise_var));
 				All_SINR(index_runs,i,Receiver_type+1,Noise_var) = SINR_i;
 			end %end noise_var
 		end %end receiver type
@@ -25,9 +25,8 @@ end %end index_runs
 All_SINR=sum(All_SINR,1)./Num_of_runs;
 
 
-
 %analysis of results
-semilogy(SNRdB_rng,squeeze(All_SINR(1,30,1,1:end)),SNRdB_rng,squeeze(All_SINR(1,30,2,1:end)),SNRdB_rng,squeeze(All_SINR(1,30,N+1,1:end)));
+plot(SNRdB_rng,10*log10(squeeze(All_SINR(1,3,1,1:end))),SNRdB_rng,10*log10(squeeze(All_SINR(1,3,2,1:end))),SNRdB_rng,10*log10(squeeze(All_SINR(1,3,N+1,1:end))));
 legend('MMSE','MRC','ZF')
 
 
